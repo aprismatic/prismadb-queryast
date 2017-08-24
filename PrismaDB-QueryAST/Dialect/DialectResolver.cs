a71a19e063f702c923782dca99aff422ef363158
bazzilic <bazzilic@gmail.com>
2017-08-24 17:31:45 +0800

    Preliminary work for introducing SQl dialects

---
+++ PrismaDB-QueryAST/Dialect/DialectResolver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrismaDB.QueryAST
+{
+    public static class DialectResolver
+    {
+        private static IDialect _dialect;
+        public static IDialect Dialect
+        {
+            get
+            {
+                if(_dialect == null)
+                    throw new ApplicationException("Dialect should be set before using the AST classes.");
+                return _dialect;
+            }
+            set => _dialect = value;
+        }
+    }
+}
---
+++ PrismaDB-QueryAST/Dialect/IDialect.cs
@@ -0,0 +1,30 @@
+﻿using PrismaDB.QueryAST.DDL;
+using PrismaDB.QueryAST.DML;
+
+namespace PrismaDB.QueryAST
+{
+    public interface IDialect
+    {
+        string IdentifierToString(Identifier q);
+        string ColumnDefinitionToString(ColumnDefinition q);
+        string CreateIndexQueryToString(CreateIndexQuery q);
+        string CreateTableQueryToString(CreateTableQuery q);
+        string TableRefToString(TableRef q);
+        string WhereClauseToString(WhereClause q);
+        string UpdateQueryToString(UpdateQuery q);
+        string SelectQueryToString(SelectQuery q);
+        string DeleteQueryToString(DeleteQuery q);
+        string InsertQueryToString(InsertQuery q);
+        string IntConstantToString(IntConstant q);
+        string BinaryConstantToString(BinaryConstant q);
+        string StringConstantToString(StringConstant q);
+        string AdditionToString(Addition q);
+        string MultiplicationToString(Multiplication q);
+        string PaillierAdditionToString(PaillierAddition q);
+        string ElGamalMultiplicationToString(ElGamalMultiplication q);
+        string ColumnRefToString(ColumnRef q);
+        string BooleanTrueToString(BooleanTrue q);
+        string BooleanInToString(BooleanIn q);
+        string BooleanEqualsToString(BooleanEquals q);
+    }
+}
