0719481b096248fe6b85bc00a5464bd870003ae1
Calvin Che <calvin@cczy.io>
2018-05-24 16:43:54 +0800

    Change Tuple in OrderByClause to Commons.Pair

--- PrismaDB-QueryAST/DML/OrderByClause.cs
+++ PrismaDB-QueryAST/DML/OrderByClause.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PrismaDB.Commons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,18 +7,18 @@ namespace PrismaDB.QueryAST.DML
 {
     public class OrderByClause : ICloneable
     {
-        public List<Tuple<ColumnRef, OrderDirection>> OrderColumns;
+        public List<Pair<ColumnRef, OrderDirection>> OrderColumns;
 
         public OrderByClause()
         {
-            OrderColumns = new List<Tuple<ColumnRef, OrderDirection>>();
+            OrderColumns = new List<Pair<ColumnRef, OrderDirection>>();
         }
 
         public OrderByClause(OrderByClause other)
         {
-            OrderColumns = new List<Tuple<ColumnRef, OrderDirection>>(other.OrderColumns.Capacity);
+            OrderColumns = new List<Pair<ColumnRef, OrderDirection>>(other.OrderColumns.Capacity);
             OrderColumns.AddRange(other.OrderColumns.Select(
-                x => new Tuple<ColumnRef, OrderDirection>(x.Item1.Clone() as ColumnRef, x.Item2)));
+                x => new Pair<ColumnRef, OrderDirection>(x.First.Clone() as ColumnRef, x.Second)));
         }
 
         public object Clone()
@@ -32,7 +33,7 @@ namespace PrismaDB.QueryAST.DML
 
         public List<ColumnRef> GetOrderByColumns()
         {
-            var orderByCols = OrderColumns.SelectMany(x => x.Item1.GetColumns());
+            var orderByCols = OrderColumns.SelectMany(x => x.First.GetColumns());
             return orderByCols.Distinct().ToList();
         }
     }
