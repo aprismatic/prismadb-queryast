965264e1a709bb6711736ef6ea51c739c7a28ed7
Calvin Che <calvin@cczy.io>
2018-09-02 17:26:32 +0800

    RegisterUserCommand

---
+++ PrismaDB-QueryAST/DCL/RegisterUserCommand.cs
@@ -0,0 +1,34 @@
+﻿using PrismaDB.QueryAST.DML;
+
+namespace PrismaDB.QueryAST.DCL
+{
+    public class RegisterUserCommand : Command
+    {
+        public StringConstant UserId;
+        public StringConstant Password;
+
+        public RegisterUserCommand()
+        {
+            UserId = new StringConstant("");
+            Password = new StringConstant("");
+        }
+
+        public RegisterUserCommand(string userId, string password)
+        {
+            UserId = new StringConstant(userId);
+            Password = new StringConstant(password);
+        }
+
+        public override string ToString()
+        {
+            return DialectResolver.Dialect.RegisterUserCommandToString(this);
+        }
+
+        public override object Clone()
+        {
+            var clone = new RegisterUserCommand(UserId.strvalue, Password.strvalue);
+
+            return clone;
+        }
+    }
+}
