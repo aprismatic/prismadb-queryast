bae72c360f84374c90bb450b05c86b8a64c1d111
Calvin Che <calvin@cczy.io>
2018-09-28 14:57:08 +0800

    Update keys, encrypt and decrypt commands (#37)

---
+++ PrismaDB-QueryAST/DCL/DecryptColumnCommand.cs
@@ -0,0 +1,36 @@
+﻿using PrismaDB.QueryAST.DML;
+
+namespace PrismaDB.QueryAST.DCL
+{
+    public class DecryptColumnCommand : Command
+    {
+        public ColumnRef Column;
+
+        public DecryptColumnCommand()
+        {
+            Column = new ColumnRef("");
+        }
+
+        public DecryptColumnCommand(string columnName)
+        {
+            Column = new ColumnRef(columnName);
+        }
+
+        public DecryptColumnCommand(ColumnRef column)
+        {
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return DialectResolver.Dialect.DecryptColumnCommandToString(this);
+        }
+
+        public override object Clone()
+        {
+            var clone = new DecryptColumnCommand((ColumnRef)Column.Clone());
+
+            return clone;
+        }
+    }
+}
