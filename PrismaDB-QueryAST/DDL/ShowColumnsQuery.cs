9c10222633abb1bb2266cd1aaf1a928f8e7edab1
Calvin Che <calvin@cczy.io>
2019-02-27 01:17:02 +0800

    SHOW COLUMNS instead of DESCRIBE, for future expandability

--- PrismaDB-QueryAST/DDL/ShowColumnsQuery.cs
+++ PrismaDB-QueryAST/DDL/ShowColumnsQuery.cs
@@ -1,32 +1,32 @@
 ﻿namespace PrismaDB.QueryAST.DDL
 {
-    public class DescribeQuery : DdlQuery
+    public class ShowColumnsQuery : DdlQuery
     {
         public TableRef TableName;
 
-        public DescribeQuery() : this("") { }
+        public ShowColumnsQuery() : this("") { }
 
-        public DescribeQuery(string tableName)
+        public ShowColumnsQuery(string tableName)
         {
             TableName = new TableRef(tableName);
         }
 
-        public DescribeQuery(TableRef table)
+        public ShowColumnsQuery(TableRef table)
             : this(table.Table.id) { }
 
-        public DescribeQuery(DescribeQuery other)
+        public ShowColumnsQuery(ShowColumnsQuery other)
         {
             TableName = other.TableName.Clone();
         }
 
         public override string ToString()
         {
-            return DialectResolver.Dialect.DescribeQueryToString(this);
+            return DialectResolver.Dialect.ShowColumnsQueryToString(this);
         }
 
         public override object Clone()
         {
-            return new DescribeQuery(this);
+            return new ShowColumnsQuery(this);
         }
     }
 }
--- PrismaDB-QueryAST/Dialect/IDialect.cs
+++ PrismaDB-QueryAST/Dialect/IDialect.cs
@@ -12,8 +12,8 @@ namespace PrismaDB.QueryAST
         string CreateTableQueryToString(CreateTableQuery q);
         string DropTableQueryToString(DropTableQuery q);
         string AlterTableQueryToString(AlterTableQuery q);
-        string ShowQueryToString(ShowQuery q);
-        string DescribeQueryToString(DescribeQuery q);
+        string ShowTablesQueryToString(ShowTablesQuery q);
+        string ShowColumnsQueryToString(ShowColumnsQuery q);
         string AlteredColumnToString(AlteredColumn q);
         string TableRefToString(TableRef q);
         string DatabaseRefToString(DatabaseRef q);
