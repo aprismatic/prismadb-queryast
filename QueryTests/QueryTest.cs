91a38e11c6e7e21845ce9984d25c1b4fdc00829b
Calvin Che <calvin@cczy.io>
2019-04-12 02:56:04 +0800

    Improve robustness of expression classes

--- QueryTests/QueryTest.cs
+++ QueryTests/QueryTest.cs
@@ -18,21 +18,21 @@ namespace QueryTests
         public void FunctionEquals()
         {
             var func1 = new ScalarFunction("func");
-            func1.Parameters.Add(new IntConstant(1));
-            func1.Parameters.Add(new StringConstant("abc"));
+            func1.AddChild(new IntConstant(1));
+            func1.AddChild(new StringConstant("abc"));
 
             var func2 = new ScalarFunction("func");
-            func2.Parameters.Add(new IntConstant(1));
-            func2.Parameters.Add(new StringConstant("abc"));
+            func2.AddChild(new IntConstant(1));
+            func2.AddChild(new StringConstant("abc"));
             Assert.Equal(func1, func2);
 
             var func3 = new ScalarFunction("func");
-            func3.Parameters.Add(new StringConstant("abc"));
-            func3.Parameters.Add(new IntConstant(1));
+            func3.AddChild(new StringConstant("abc"));
+            func3.AddChild(new IntConstant(1));
             Assert.NotEqual(func1, func3);
 
             var func4 = (ScalarFunction)func2.Clone();
-            func4.Parameters[0] = new StringConstant("123");
+            func4.AddChild(0, new StringConstant("123"));
 
             Assert.NotEqual(func2, func4);
 
@@ -40,11 +40,11 @@ namespace QueryTests
             Assert.NotEqual(func1, func5);
 
             var func6 = new ScalarFunction("func");
-            func6.Parameters.Add(new IntConstant(1));
+            func6.AddChild(new IntConstant(1));
             Assert.NotEqual(func1, func6);
 
             var func7 = new ScalarFunction("func");
-            func7.Parameters.Add(new ColumnRef("tt", "col"));
+            func7.AddChild(new ColumnRef("tt", "col"));
             Assert.Single(func7.GetColumns());
             Assert.Equal(new ColumnRef("tt", "col"), func7.GetColumns()[0]);
         }
@@ -54,11 +54,11 @@ namespace QueryTests
         {
             {
                 var func = new SumAggregationFunction("fn");
-                func.Parameters.Add(new ColumnRef("a"));
+                func.AddChild(new ColumnRef("a"));
 
                 var func2 = new SumAggregationFunction("fn");
                 Assert.NotEqual(func, func2);
-                func2.Parameters.Add(new ColumnRef("a"));
+                func2.AddChild(new ColumnRef("a"));
                 Assert.Equal(func, func2);
 
                 var func3 = func.Clone() as SumAggregationFunction;
@@ -66,11 +66,11 @@ namespace QueryTests
             }
             {
                 var func = new CountAggregationFunction("fn");
-                func.Parameters.Add(new ColumnRef("a"));
+                func.AddChild(new ColumnRef("a"));
 
                 var func2 = new CountAggregationFunction("fn");
                 Assert.NotEqual(func, func2);
-                func2.Parameters.Add(new ColumnRef("a"));
+                func2.AddChild(new ColumnRef("a"));
                 Assert.Equal(func, func2);
 
                 var func3 = func.Clone() as CountAggregationFunction;
@@ -78,11 +78,11 @@ namespace QueryTests
             }
             {
                 var func = new AvgAggregationFunction("fn");
-                func.Parameters.Add(new ColumnRef("a"));
+                func.AddChild(new ColumnRef("a"));
 
                 var func2 = new AvgAggregationFunction("fn");
                 Assert.NotEqual(func, func2);
-                func2.Parameters.Add(new ColumnRef("a"));
+                func2.AddChild(new ColumnRef("a"));
                 Assert.Equal(func, func2);
 
                 var func3 = func.Clone() as AvgAggregationFunction;
