c80a530ca9a6086f2a44e68e25eb0967654e6c67
Calvin Che <calvin@cczy.io>
2019-05-22 16:01:26 +0800

    Add WITH VALUES to REBALANCE OPETREE

--- PrismaDB-QueryAST/DCL/RebalanceOpetreeCommand.cs
+++ PrismaDB-QueryAST/DCL/RebalanceOpetreeCommand.cs
@@ -1,15 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using PrismaDB.QueryAST.DML;
+using System.Collections.Generic;
 
 namespace PrismaDB.QueryAST.DCL
 {
     public class RebalanceOpetreeCommand : Command
     {
-        public RebalanceOpetreeCommand() { }
+        public List<Constant> WithValues;
+
+        public RebalanceOpetreeCommand()
+        {
+            WithValues = new List<Constant>();
+        }
+        public RebalanceOpetreeCommand(List<Constant> withValues)
+        {
+            WithValues = new List<Constant>();
+            foreach (var value in withValues)
+                WithValues.Add(value);
+        }
 
         public override List<TableRef> GetTables() => new List<TableRef>();
 
         public override string ToString() => DialectResolver.Dialect.RebalanceOpetreeCommandToString(this);
 
-        public override object Clone() => new UpdateKeysCommand();
+        public override object Clone() => new RebalanceOpetreeCommand(WithValues);
     }
 }
